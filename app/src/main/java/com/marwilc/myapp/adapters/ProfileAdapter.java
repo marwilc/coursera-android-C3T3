e3ec9bef0d01fdd31f7a6b98a6433a5df181a170
Marwil Campos <marwilcampos@gmail.com>
2017-05-19 20:11:56 -0400

    release ver 1.2

---
+++ app/src/main/java/com/marwilc/myapp/adapters/ProfileAdapter.java
@@ -0,0 +1,65 @@
+package com.marwilc.myapp.adapters;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.marwilc.myapp.R;
+import com.marwilc.myapp.fragments.RecyclerViewFragment;
+import com.marwilc.myapp.modelData.Pet;
+
+import java.util.ArrayList;
+
+/**
+ * Created by marwilc on 19/05/17.
+ */
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>{
+    ArrayList<Pet> pets;
+    Activity activity;
+
+
+    public ProfileAdapter(ArrayList<Pet> pets, Activity activity) {
+        this.pets = pets;
+        this.activity = activity;
+    }
+
+    @Override
+    public ProfileViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_avatar2,parent,false);
+        return new ProfileViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ProfileViewHolder holder, int position) {
+
+        final Pet pet = pets.get(position);
+        String likes= Integer.toString(pet.getLikes());
+        holder.ivPicture.setImageResource(pet.getPicture());
+        holder.tvlikes.setText(likes);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return pets.size();
+    }
+
+    public static class ProfileViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView ivPicture;
+        private TextView tvlikes;
+
+
+        public ProfileViewHolder(View itemView) {
+            super(itemView);
+
+            ivPicture   = (ImageView) itemView.findViewById(R.id.imgAvatarCv2);
+            tvlikes     = (TextView) itemView.findViewById(R.id.tvNumberCv2);
+        }
+    }
+}
--- app/src/main/java/com/marwilc/myapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/marwilc/myapp/fragments/ProfileFragment.java
@@ -8,9 +8,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.marwilc.myapp.R;
 import com.marwilc.myapp.adapters.ProfileAdapter;
+import com.marwilc.myapp.modelData.Pet;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +24,7 @@ public class ProfileFragment extends Fragment {
 
 
     private RecyclerView rvProfile;
+    private ArrayList<Pet> alPets;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,17 +34,35 @@ public class ProfileFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_profile, container,false);
 
         rvProfile = (RecyclerView) v.findViewById(R.id.rvAvatars2);
-
+        CircularImageView civProfile    = (CircularImageView) v.findViewById(R.id.ivAvatarFragmentProfile);
+        TextView tvName                 = (TextView) v.findViewById(R.id.tvNameFragmentProfile);
+        
         GridLayoutManager glmGrid = new GridLayoutManager(getActivity(), 3);
         rvProfile.setLayoutManager(glmGrid);
 
+        initProfileList();
         initAdapter();
+
+        civProfile.setImageResource(alPets.get(0).getPicture());
+        tvName.setText(alPets.get(0).getName());
+
         return v;
     }
 
+
+    public void initProfileList(){
+
+        alPets = new ArrayList<>();
+        alPets.add(new Pet("Steam", 5, R.drawable.dog1));
+        alPets.add(new Pet("Pitus", 7, R.drawable.dog1));
+        alPets.add(new Pet("Fido", 8, R.drawable.dog1));
+        alPets.add(new Pet("Try", 9, R.drawable.dog1));
+        alPets.add(new Pet("Reapper", 0, R.drawable.dog1));
+        alPets.add(new Pet("Tin", 1, R.drawable.dog1));
+    }
     public void initAdapter(){
 
-        ProfileAdapter adapter = new ProfileAdapter(alPetProfiles, getActivity());
+        ProfileAdapter adapter = new ProfileAdapter(alPets, getActivity());
         rvProfile.setAdapter(adapter);
 
 
